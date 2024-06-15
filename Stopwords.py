bee636ce2c53d63efea25641c66d36aefdbe1053
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2024-06-15 13:05:26 -0400

    Added a new method to Stopwords.py to filter text input and lemmatize words according to the context.

--- Stopwords.py
+++ Stopwords.py
@@ -1,11 +1,14 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 # downloading the stopwords from nltk
 nltk.download('stopwords')
 # donwloading the resource word_tokenize
 nltk.download('punkt')
+# donwloading the resource wordnet
+nltk.download('wordnet')
 
 # setting the stopwords dictionary to english language
 stop_words_es = set(stopwords.words('english'))
@@ -18,5 +21,18 @@ def find_stopwords(text):
     # joining the text again 
     return " ".join(filtered_words)
 
-text = "This is a example text to find the stopwords."
-print(find_stopwords(text))
+
+def filter_review(reviews):
+  #Tokenize the text
+  tokens = word_tokenize(reviews)
+
+  #Remove stop words
+  filtered_tokens = [token for token in tokens if token not in stop_words_es]
+
+  #Lemmatize the tokens (to mantain the context)
+  lemmatizer = WordNetLemmatizer()
+  lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+  #Join the tokens back into a string
+  processed_text = ' '.join(lemmatized_tokens)
+  return processed_text
