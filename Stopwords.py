4ba568ade971bc291563dd420645b4eb952862c8
Nicolas Vargas Rojas <nicovarojas@gmail.com>
2024-06-16 10:57:29 -0400

    Added more words to discard from the dataset

--- Stopwords.py
+++ Stopwords.py
@@ -11,28 +11,25 @@ nltk.download('punkt')
 nltk.download('wordnet')
 
 # setting the stopwords dictionary to english language
-stop_words_es = set(stopwords.words('english'))
-
-def find_stopwords(text):
-    # tokenizing the text 
-    words = word_tokenize(text)
-    # filtering the stopwords from the text
-    filtered_words = [word for word in words if word not in stop_words_es]
-    # joining the text again 
-    return " ".join(filtered_words)
-
+stop_words_en = set(stopwords.words('english'))
+# Define additional stopwords
+additional_exclusions = {'persona', 'team', 'minecraft', 'arma', 'bean', 'boss','ps4','wild','moon','2d','wasteland','ultra','military','editor','allows','solid','journey','expert','bungie','season','terraria','minor','hunting','kojima','deck','destiny','remaster','complain','fall','slay','gta','atlus','spire','terrarium','rockstar','biome','mafia','castle',
+                         'access','instead','idea','sorry','ca','dlc','dayz','until','valve','tutorial','concept','swing','horde','regionlockchine','unless','sims','ark','was','company','customer','state','software','advertised','region','survivor','2016','fighter','headshot','zombie','alpha','trailer','planet','pubg','microtransactions','payday','too','camping','ubisoft','galaxy','rome','skyrim','vr','h1z1','vegas','warband','flight','creation','forum','netcode','sky','elite','china','chinese','hero','policy','pile','untill','zombies','xcom','rust','capcom'}
 
 def filter_review(reviews):
   #Tokenize the text
   tokens = word_tokenize(reviews)
 
   #Remove stop words
-  filtered_tokens = [token for token in tokens if token not in stop_words_es]
+  filtered_tokens = [token for token in tokens if token not in stop_words_en]
 
   #Lemmatize the tokens (to mantain the context)
   lemmatizer = WordNetLemmatizer()
   lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+  
+  # Remove extra words after lemmatizer
+  final_tokens = [token for token in lemmatized_tokens if token not in additional_exclusions]
 
   #Join the tokens back into a string
-  processed_text = ' '.join(lemmatized_tokens)
+  processed_text = ' '.join(final_tokens)
   return processed_text
