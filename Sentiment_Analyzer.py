71730f6db0ba6014df7b6deaabdc8e0aedbedc7b
Nicolas Vargas Rojas <nicovarojas@gmail.com>
2024-06-15 21:14:30 -0400

    Showing the distribution of reviews

--- Sentiment_Analyzer.py
+++ Sentiment_Analyzer.py
@@ -8,4 +8,11 @@ df_training = pd.read_csv('dataset_training.csv')
 
 reviews = df_training['review'].astype(str)
 filtered_reviews = reviews.apply(filter_review)
-print(filtered_reviews)
+#print(filtered_reviews)
+
+df_filtered = pd.DataFrame(filtered_reviews, columns=['review'])
+df_filtered['voted_up'] = df_training['voted_up'].astype(int)
+print(df_filtered)
+
+plt.hist(df_filtered['voted_up'])
+plt.show()
