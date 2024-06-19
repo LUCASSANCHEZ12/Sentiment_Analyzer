619571d42e97fcba5bb88fc966489d5ea8de42f1
Nicolas Vargas Rojas <nicovarojas@gmail.com>
2024-06-19 13:24:33 -0400

    Changed comments to English

--- backend/api.py
+++ backend/api.py
@@ -1,26 +1,33 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import pandas as pd
 
 app = FastAPI()
 
-# Permitir CORS para la comunicación con el frontend de Angular
+#Allow CORS for communication with Angular frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Cambia según la URL del frontend
+    allow_origins=["http://localhost:4200"],  #Changes based on frontend URL
+    allow_credentials=True,
+
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-class Item(BaseModel):
-    name: str
-    description: str
+df_training = pd.read_csv('./../dataset_training.csv')
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI"}
 
-@app.post("/items/")
-def create_item(item: Item):
-    return {"item": item}
+reviews = df_training['review'].astype(str)
+
+df_filtered = pd.DataFrame(reviews, columns=['review'])
+df_filtered['voted_up'] = df_training['voted_up'].astype(int)
+
+class Review(BaseModel):
+    review: str
+
+@app.get("/reviews/")
+def get_reviews():
+    return df_filtered.to_dict(orient='records')
